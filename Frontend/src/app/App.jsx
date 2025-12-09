fe7f320118a936eea5403541a2deb64c7d7fc59d
Duong Phu Dong <dongduong840@gmail.com>
2025-12-09 11:58:06 +0700

    update: change Routes & import Login, Register using Modules Folder

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from '../pages/home';
-import { LoginPage, RegisterPage } from '../pages/auth';
+import { LoginPage, RegisterPage } from '../modules/auth';
 
 function App() {
   return (
