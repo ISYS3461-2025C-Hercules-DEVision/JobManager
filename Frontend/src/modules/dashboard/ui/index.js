bc26aa218e56e05ad415f4fd70d2b5cd292bf911
Duong Phu Dong <dongduong840@gmail.com>
2025-12-15 17:30:17 +0700

    feat: create index.js

---
+++ Frontend/src/modules/dashboard/ui/index.js
@@ -0,0 +1,9 @@
+export { default as DashboardLayout } from './DashboardLayout';
+export { default as Sidebar } from './Sidebar';
+export { default as DashboardPage } from './DashboardPage';
+export { default as FindApplicantsPage } from './FindApplicantsPage';
+export { default as PostManagerPage } from './PostManagerPage';
+export { default as JobPostPage } from './JobPostPage';
+export { default as SettingsPage } from './SettingsPage';
+export { default as ProtectedRoute } from './ProtectedRoute';
+
