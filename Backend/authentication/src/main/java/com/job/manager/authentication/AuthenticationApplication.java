28b9944acb440f93bed5b15c349b5a17ff56ddcc
HoHuy2402 <hoquanghuyhhh@gmail.com>
2025-12-22 23:29:33 +0700

    update

--- Backend/authentication/src/main/java/com/job/manager/authentication/AuthenticationApplication.java
+++ Backend/authentication/src/main/java/com/job/manager/authentication/AuthenticationApplication.java
@@ -2,11 +2,9 @@ package com.job.manager.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthenticationApplication {
 
